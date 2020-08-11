e710e5806e262b3b67ea59060acfdc678dfce3e2
jemtca <30645648+jemtca@users.noreply.github.com>
2020-08-10 23:36:51 -0700

    CodingBat

---
+++ JavaScript/Warmup-1/sleepIn.js
@@ -0,0 +1,13 @@
+
+// the parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation
+// we sleep in if it is not a weekday or we're on vacation
+// return true if we sleep in
+const sleepIn = (weekday, vacation) => {
+
+    return (!weekday || vacation) ? true : false;
+
+}
+
+console.log(sleepIn(false, false));
+console.log(sleepIn(true, false));
+console.log(sleepIn(false, true));
