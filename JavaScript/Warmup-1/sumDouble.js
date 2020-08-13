744177aa611649e9f6c93bf6d1260f769fdd1f37
jemtca <30645648+jemtca@users.noreply.github.com>
2020-08-12 22:16:31 -0700

    CodingBat

---
+++ JavaScript/Warmup-1/sumDouble.js
@@ -0,0 +1,11 @@
+
+// given two int values, return their sum. Unless the two values are the same, then return double their sum
+const sumDouble = (a, b) => {
+
+    return a == b ? (a + b) * 2 : a + b;
+
+}
+
+console.log(sumDouble(1, 2));
+console.log(sumDouble(3, 2));
+console.log(sumDouble(2, 2));
