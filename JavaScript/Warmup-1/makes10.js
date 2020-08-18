61a5ef2b1dd91492b3765c0602114900dfdc6289
jemtca <30645648+jemtca@users.noreply.github.com>
2020-08-17 23:52:11 -0700

    CodingBat

---
+++ JavaScript/Warmup-1/makes10.js
@@ -0,0 +1,11 @@
+
+// given 2 ints, a and b, return true if one of them is 10 or if their sum is 10
+const makes10 = (a, b) => {
+
+return (a === 10 || b === 10) || a + b === 10 ? true : false;
+
+}
+
+console.log(makes10(9, 10));
+console.log(makes10(9, 9));
+console.log(makes10(1, 9));
