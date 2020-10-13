e373f07ad9755c95d14d5b941b821d77d37dc8a9
jemtca <30645648+jemtca@users.noreply.github.com>
2020-10-12 22:47:23 -0700

    CodingBat

---
+++ JavaScript/Warmup-1/backAround.js
@@ -0,0 +1,12 @@
+
+// given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt"
+// he original string will be length 1 or more
+const backAround = (str) => {
+
+    return str[str.length - 1] + str + str[str.length - 1];
+
+}
+
+console.log(backAround('cat'));
+console.log(backAround('Hello'));
+console.log(backAround('a'));
