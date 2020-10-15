54c73f23c843fde0e0e978514b33d99034906c4e
jemtca <30645648+jemtca@users.noreply.github.com>
2020-10-14 23:17:05 -0700

    CodingBat

---
+++ JavaScript/Warmup-1/front22.js
@@ -0,0 +1,16 @@
+
+// given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields "kikittenki"
+// if the string length is less than 2, use whatever chars are there
+const front22 = (str) => {
+
+    if (str.length < 2) {
+        return str + str + str;
+    } else {
+        return str.substring(0, 2) + str + str.substring(0,2);
+    }
+
+}
+
+console.log(front22('kitten'));
+console.log(front22('Ha'));
+console.log(front22('abc'));
