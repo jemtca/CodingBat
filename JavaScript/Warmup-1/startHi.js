184c923200863fe7745814c3b897d60c138c0d60
jemtca <30645648+jemtca@users.noreply.github.com>
2020-10-15 23:52:44 -0700

    CodingBat

---
+++ JavaScript/Warmup-1/startHi.js
@@ -0,0 +1,11 @@
+
+// given a string, return true if the string starts with "hi" and false otherwise
+const startHi = (str) => {
+
+    return str.startsWith('hi'.toLowerCase()) ? true : false;
+
+}
+
+console.log(startHi('hi there'));
+console.log(startHi('hi'));
+console.log(startHi('hello hi'));
