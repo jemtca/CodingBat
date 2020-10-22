92bba53648a8951567755c82b9fab57c70ec9f04
jemtca <30645648+jemtca@users.noreply.github.com>
2020-10-21 23:56:49 -0700

    CodingBat

---
+++ JavaScript/Warmup-1/loneTeen.js
@@ -0,0 +1,12 @@
+
+// we'll say that a number is "teen" if it is in the range 13..19 inclusive
+// given 2 int values, return true if one or the other is teen, but not both
+const loneTeent = (a, b) => {
+
+
+
+}
+
+console.log(loneTeent(13, 99));
+console.log(loneTeent(21, 19));
+console.log(loneTeent(13, 13));
