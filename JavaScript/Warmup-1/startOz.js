9dac04128fce65910117f0195f2ed1f2f5f499f6
jemtca <30645648+jemtca@users.noreply.github.com>
2020-10-28 23:28:43 -0700

    CodingBat

---
+++ JavaScript/Warmup-1/startOz.js
@@ -0,0 +1,22 @@
+
+// given a string, return a string made of the first 2 chars (if present)
+// however include first char only if it is 'o' and include the second only if it is 'z', so "ozymandias" yields "oz"
+const startOz = (str) => {
+
+    result = '';
+
+    if (str.length >= 1 && str[0] === 'o') {
+        result = result + 'o';
+    }
+
+    if (str.length >= 2 && str[1] === 'z') {
+        result = result + 'z';
+    }
+
+    return result;
+
+}
+
+console.log(startOz('ozymandias'));
+console.log(startOz('bzoo'));
+console.log(startOz('oxx'));
