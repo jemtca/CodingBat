015c9f34648e6cb844a7171c1e646e8842a75bd4
jemtca <30645648+jemtca@users.noreply.github.com>
2020-10-29 23:52:16 -0700

    CodingBat

---
+++ JavaScript/Warmup-1/intMax.js
@@ -0,0 +1,17 @@
+
+// given three int values, a b c, return the largest
+const intMax = (a, b, c) => {
+
+    if (a >= b && a >= c) {
+        return a;
+    } else if (b >= a && b >= c) {
+        return b;
+    } else {
+        return c;
+    }
+
+}
+
+console.log(intMax(1, 2, 3));
+console.log(intMax(1, 3, 2));
+console.log(intMax(3, 2, 1));
