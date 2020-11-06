a80c53285ea9114b4919d7976ab4ed41a1f8a745
jemtca <30645648+jemtca@users.noreply.github.com>
2020-11-05 23:22:41 -0800

    CodingBat

---
+++ JavaScript/Warmup-1/lastDigit.js
@@ -0,0 +1,11 @@
+
+// given two non-negative int values, return true if they have the same last digit, such as with 27 and 57
+const lastDigit = (a, b) => {
+
+    return a % 10 === b % 10 ? true : false;
+
+}
+
+console.log(lastDigit(7, 17));
+console.log(lastDigit(6, 17));
+console.log(lastDigit(3, 113));
