264f4a1c9c340793251ece197704901a4155b087
jemtca <30645648+jemtca@users.noreply.github.com>
2020-11-17 23:58:12 -0800

    CodingBat

---
+++ JavaScript/Warmup-1/stringTimes.js
@@ -0,0 +1,17 @@
+
+// given a string and a non-negative int n, return a larger string that is n copies of the original string
+const stringTimes = (str, n) => {
+
+    let newStr = '';
+
+    for (let i = 0; i < n; i++) {
+        newStr = newStr + str;
+    }
+
+    return newStr;
+
+}
+
+console.log(stringTimes('Hi', 2));
+console.log(stringTimes('Hi', 3));
+console.log(stringTimes('Hi', 1));
