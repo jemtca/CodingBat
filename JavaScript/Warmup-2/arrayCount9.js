6ff80d4123f304d4026a4e7a7e949d90c47cdc88
jemtca <30645648+jemtca@users.noreply.github.com>
2020-12-01 22:36:31 -0800

    CodingBat

---
+++ JavaScript/Warmup-2/arrayCount9.js
@@ -0,0 +1,19 @@
+
+// given an array of ints, return the number of 9's in the array
+const arrayCount9 = (nums) => {
+
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 9) {
+            count++;
+        }
+    }
+
+    return count;
+
+};
+
+console.log(arrayCount9([1, 2, 9]));
+console.log(arrayCount9([1, 9, 9]));
+console.log(arrayCount9([1, 9, 9, 3, 9]));
