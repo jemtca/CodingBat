153bc83fa368d69a192bac9ddd9b9cc8739a5079
jemtca <30645648+jemtca@users.noreply.github.com>
2020-12-02 23:27:39 -0800

    CodingBat

---
+++ JavaScript/Warmup-2/arrayFront9.js
@@ -0,0 +1,22 @@
+
+// given an array of ints, return true if one of the first 4 elements in the array is a 9
+// the array length may be less than 4
+const arrayFront9 = (nums) => {
+
+    let answer = false;
+    let found = false;
+
+    for (let i = 0; i < nums.length && !found; i++) {
+        if (i < 4 && nums[i] === 9) {
+            answer = true;
+            found = true;
+        }
+    }
+
+    return answer;
+
+};
+
+console.log(arrayFront9([1, 2, 9, 3, 4]));
+console.log(arrayFront9([1, 2, 3, 4, 9]));
+console.log(arrayFront9([1, 2, 3, 4, 5]));
