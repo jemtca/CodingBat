ea7cf173debe0071e44cbcc04d2df64d0c47e3af
jemtca <30645648+jemtca@users.noreply.github.com>
2020-12-09 15:52:28 -0800

    CodingBat

---
+++ JavaScript/Warmup-2/altPairs.js
@@ -0,0 +1,23 @@
+
+// given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien"
+const altPairs = (str) => {
+
+    let newStr = '';
+
+    for (let i = 0; i < str.length; i = i + 4) {
+        let end = i + 2;
+
+        if (end > str.length) {
+            end = str.length;
+        }
+
+        newStr = newStr + str.substring(i, end);
+    }
+
+    return newStr;
+
+};
+
+console.log(altPairs('kitten'));
+console.log(altPairs('Chocolate'));
+console.log(altPairs('CodingHorror'));
