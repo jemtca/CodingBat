1a0a853fe9e3d21725753bd87c7a3babb68a451b
jemtca <30645648+jemtca@users.noreply.github.com>
2020-12-08 22:42:31 -0800

    CodingBat

---
+++ JavaScript/Warmup-2/stringX.js
@@ -0,0 +1,23 @@
+
+// given a string, return a version where all the "x" have been removed
+// except an "x" at the very start or end should not be removed
+const stringX = (str) => {
+
+    let newMiddleString = '';
+    
+    if (str.length > 1) {
+        for (let i = 1; i < str.length - 1; i++) {
+            if (str[i] !== 'x') {
+                newMiddleString = newMiddleString + str[i];
+            }
+        }
+        return str[0] + newMiddleString + str[str.length - 1];
+    } else {
+        return str;
+    }
+
+};
+
+console.log(stringX('xxHxix'));
+console.log(stringX('abxxxcd'));
+console.log(stringX('xabxxxcdx'));
