8de2f640c955a951cc157377ce801fda6374a8b6
jemtca <30645648+jemtca@users.noreply.github.com>
2020-12-15 23:48:54 -0800

    CodingBat

---
+++ JavaScript/Warmup-2/array667.js
@@ -0,0 +1,20 @@
+
+// given an array of ints, return the number of times that two 6's are next to each other in the array
+// also count instances where the second "6" is actually a 7
+const array667 = (nums) => {
+
+    let count = 0;
+
+    for (let i = 0; i < nums.length - 1; i++ ) {
+        if (nums[i] === 6 && (nums[i + 1] === 6 || nums[i + 1] === 7)) {
+            count++;
+        }
+    }
+
+    return count;
+
+};
+
+console.log(array667([6, 6, 2]));
+console.log(array667([6, 6, 2, 6]));
+console.log(array667([6, 7, 2, 6]));
