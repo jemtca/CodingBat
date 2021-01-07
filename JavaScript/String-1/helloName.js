748fcd16bd15b22140f1ff01f04df2ce72f5af89
jemtca <30645648+jemtca@users.noreply.github.com>
2021-01-06 23:34:26 -0800

    CodingBat

--- JavaScript/String-1/helloName.js
+++ JavaScript/String-1/helloName.js
@@ -1,2 +1,9 @@
 
-// 
+// given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!"
+const helloName = (name) => {
+    return `Hello ${name}!`
+};
+
+console.log(helloName('Bob'));
+console.log(helloName('Alice'));
+console.log(helloName('X'));
