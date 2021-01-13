90ec60a8857f18f72563e0520a12b1fa49da3eb8
jemtca <30645648+jemtca@users.noreply.github.com>
2021-01-12 22:47:23 -0800

    CodingBat

---
+++ JavaScript/String-1/makeTags.js
@@ -0,0 +1,12 @@
+
+// the web is built with HTML strings like '<i>Yay</i>' which draws Yay as italic text
+// in this example, the 'i' tag makes <i> and </i> which surround the word 'Yay'
+// given tag and word strings, create the HTML string with tags around the word
+// e.g. '<i>Yay</i>'
+const makeTags = (tag, word) => {
+    return `<${tag}>${word}</${tag}>`
+};
+
+console.log(makeTags('i', 'Yay'));
+console.log(makeTags('i', 'Hello'));
+console.log(makeTags('cite', 'Yay'));
