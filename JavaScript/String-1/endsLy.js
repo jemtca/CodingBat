c2559939e9defdcbc92d5ab59e6fe28d15217462
jemtca <30645648+jemtca@users.noreply.github.com>
2021-02-03 22:48:46 -0800

    CodingBat

---
+++ JavaScript/String-1/endsLy.js
@@ -0,0 +1,9 @@
+
+// given a string, return true if it ends in 'ly'
+const endsLy = (str) => {
+    return str.endsWith('ly');
+};
+
+console.log(endsLy('oddly'));
+console.log(endsLy('y'));
+console.log(endsLy('oddy'));
