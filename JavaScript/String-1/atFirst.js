e6789c500db5467dd308c1d0e20ceaf5923a6a1f
jemtca <30645648+jemtca@users.noreply.github.com>
2021-02-12 22:47:03 -0800

    CodingBat

--- JavaScript/String-1/atFirst.js
+++ JavaScript/String-1/atFirst.js
@@ -2,6 +2,17 @@
 // given a string, return a string length 2 made of its first 2 chars
 // if the string length is less than 2, use '@' for the missing chars
 const atFirst = (str) => {
+    let newStr = '';
+
+    if (str.length === 0) {
+        newStr = '@@';
+    } else if (str.length === 1) {
+        newStr = str[0] + '@';
+    } else {
+        newStr = str.substring(0, 2);
+    }
+
+    return newStr;
 };
 
 console.log(atFirst('hello'));
