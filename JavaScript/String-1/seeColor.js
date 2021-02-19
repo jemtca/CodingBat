15e494fe8ff823ebbdc7644b623dc16ea56b6b19
jemtca <30645648+jemtca@users.noreply.github.com>
2021-02-18 23:45:28 -0800

    CodingBat

---
+++ JavaScript/String-1/seeColor.js
@@ -0,0 +1,18 @@
+
+// given a string, if the string begins with 'red' or 'blue' return that color string
+// otherwise return the empty string
+const seeColor = (str) => {
+    
+    if (str.startsWith('red')) {
+        return 'red';
+    } else if (str.startsWith('blue')) {
+        return 'blue';
+    } else {
+        return '';
+    }
+
+};
+
+console.log(seeColor('redxx'));
+console.log(seeColor('xxred'));
+console.log(seeColor('blueTimes'));
