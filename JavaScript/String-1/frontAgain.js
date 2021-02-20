9c9bb11a63315fa4f5bccb70737e1e4ad176b768
jemtca <30645648+jemtca@users.noreply.github.com>
2021-02-19 22:30:03 -0800

    CodingBat

---
+++ JavaScript/String-1/frontAgain.js
@@ -0,0 +1,16 @@
+
+// given a string, return true if the first 2 chars in the string also appear at the end of the string
+// such as with "edited"
+const frontAgain = (str) => {
+    let result = false;
+
+    if (str.length > 1 && str.startsWith(str.substring(0, 2)) === str.endsWith(str.substring(0, 2))) {
+        result = true;
+    }
+
+    return result;
+};
+
+console.log(frontAgain('edited'));
+console.log(frontAgain('edit'));
+console.log(frontAgain('ed'));
