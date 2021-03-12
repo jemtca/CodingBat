891f4588a7f1ebceb33f2ed749e5880f17b2616a
jemtca <30645648+jemtca@users.noreply.github.com>
2021-03-11 23:58:06 -0800

    CodingBat

---
+++ JavaScript/String-1/without2.js
@@ -0,0 +1,13 @@
+
+// given a string, if a length 2 substring appears at both its beginning and end, return a string without the substring at the beginning
+// so 'HelloHe' yields 'lloHe'
+// the substring may overlap with itself
+// so 'Hi' yields '
+// otherwise, return the original string unchanged
+const without2 = (str) => {
+
+};
+
+console.log(without2('HelloHe'));
+console.log(without2('HelloHi'));
+console.log(without2('Hi'));
