ebfc90dca76e41ee8aa554a7bf57535193f21c78
jemtca <30645648+jemtca@users.noreply.github.com>
2021-03-26 21:02:18 -0700

    CodingBat

--- JavaScript/String-1/startWord.js
+++ JavaScript/String-1/startWord.js
@@ -5,7 +5,16 @@
 // so, so with the string 'hippo' the word 'hi' returns 'hi' and 'xip' returns 'hip'
 // the word will be at least length 1.
 const startWord = (str, word) => {
+    let newWord = word.substring(1);
 
+    if (str.length === 1 && word.length === 1) {
+        return str;
+    }
+    else if (str.length > 1 && str.substring(1).startsWith(newWord)) {
+        return str.substring(0, word.length);
+    } else {
+        return '';
+    }    
 };
 
 console.log(startWord('hippo', 'hi'));
