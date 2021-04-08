47f24e77f8a0bae23fad9beef26ee127c5133c14
jemtca <30645648+jemtca@users.noreply.github.com>
2021-04-07 23:34:08 -0700

    CodingBat

---
+++ JavaScript/Array-1/sum3.js
@@ -0,0 +1,9 @@
+
+// given an array of ints length 3, return the sum of all the elements
+const sum3 = (nums) => {
+    return nums[0] + nums[1] + nums[2];
+};
+
+console.log(sum3([1, 2, 3]));
+console.log(sum3([5, 11, 2]));
+console.log(sum3([7, 0 , 0]));
