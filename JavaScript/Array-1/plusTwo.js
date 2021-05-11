9154a87483d656a3b648884b2912d688c56443f7
jemtca <30645648+jemtca@users.noreply.github.com>
2021-05-10 23:22:53 -0700

    CodingBat

---
+++ JavaScript/Array-1/plusTwo.js
@@ -0,0 +1,9 @@
+
+// given 2 int arrays, each length 2, return a new array length 4 containing all their elements
+const plusTwo = (a, b) => {
+    return a.concat(b);
+};
+
+console.log(plusTwo([1, 2], [3, 4]));
+console.log(plusTwo([4, 4], [2, 2]));
+console.log(plusTwo([9, 2], [3, 4]));
