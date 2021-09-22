38183bdaaae678a7298e6eec56aced56fb38085b
jemtca <30645648+jemtca@users.noreply.github.com>
2021-09-21 21:53:44 -0700

    CodingBat

--- JavaScript/Array-1/make2.js
+++ JavaScript/Array-1/make2.js
@@ -2,7 +2,7 @@
 // given 2 int arrays, a and b, return a new array length 2 containing, as much as will fit, the elements from a followed by the elements from b
 // the arrays may be any length, including 0, but there will be 2 or more elements available between the 2 arrays
 const make2 = (a, b) => {
-    let newArray = [];
+    const newArray = [];
 
     if (a.length === 0) {
         newArray.push(b[0]);
