35a5f51dc7f1c68bb16eb4869e4a01bfa1f085f1
jemtca <30645648+jemtca@users.noreply.github.com>
2021-11-16 23:15:27 -0800

    CodingBat

---
+++ JavaScript/Array-2/fizzArray2.js
@@ -0,0 +1,15 @@
+
+// Given a number n, create and return a new string array of length n, containing the strings '0', '1' '2' .. through n-1. N may be 0, in which case just return a length 0 array
+const fizzArray2 = (n) => {
+	const result  = [];
+
+	for (let i = 0; i < n; i++) {
+		result.push(String(i));
+	}
+
+	return result;
+};
+
+console.log(fizzArray2(4));
+console.log(fizzArray2(10));
+console.log(fizzArray2(2));
