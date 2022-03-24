0d5d2afc619ba255c5e11d71865488b358747c84
jemtca <30645648+jemtca@users.noreply.github.com>
2022-03-23 21:34:01 -0700

    CodingBat

--- JavaScript/Array-3/linearIn.js
+++ JavaScript/Array-3/linearIn.js
@@ -1,2 +1,27 @@
 
-// 
+// given two arrays of ints sorted in increasing order, outer and inner, return true if all of the numbers in inner appear in outer
+// the best solution makes only a single 'linear' pass of both arrays, taking advantage of the fact that both arrays are already in sorted order
+const linearIn = (outer, inner) => {
+    let found = false;
+
+    for (let i = 0; i < inner.length; i++) {
+        for (let j = 0; j < outer.length; j++) {
+            if (inner[i] === outer[j]) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            return false;
+        } else {
+            found = false;
+        }
+    }
+
+    return true;
+};
+
+console.log(linearIn([1, 2, 4, 6], [2, 4]));
+console.log(linearIn([1, 2, 4, 6], [2, 3, 4]));
+console.log(linearIn([1, 2, 4, 4, 6], [2, 4]));
