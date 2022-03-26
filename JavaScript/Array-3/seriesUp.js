16962f0efc8253773fbb6ed02668f9e03e1824e5
jemtca <30645648+jemtca@users.noreply.github.com>
2022-03-25 21:26:49 -0700

    CodingBat

--- JavaScript/Array-3/seriesUp.js
+++ JavaScript/Array-3/seriesUp.js
@@ -1,2 +1,17 @@
 
-// 
+// given n>=0, create an array with the pattern {1,    1, 2,    1, 2, 3,   ... 1, 2, 3 .. n} (spaces added to show the grouping)
+const seriesUp = (n) => {
+    let array = [];
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j <= i; j++) {
+            array.push(j + 1);
+        }
+    }
+
+    return array;
+};
+
+console.log(seriesUp(3));
+console.log(seriesUp(4));
+console.log(seriesUp(2));
