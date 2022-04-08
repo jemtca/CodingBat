13b4031944f843aa8dfdf96326827fd02319032a
jemtca <30645648+jemtca@users.noreply.github.com>
2022-04-07 23:15:43 -0700

    CodingBat

--- JavaScript/Array-3/countClumps.js
+++ JavaScript/Array-3/countClumps.js
@@ -1,2 +1,22 @@
 
-// 
+// say that a 'clump' in an array is a series of 2 or more adjacent elements of the same value
+// return the number of clumps in the given array
+const countClumps = (nums) => {
+    let total = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i < nums.length - 2 && nums[i] === nums[i + 1] && nums[i + 1] !== nums[i + 2]) {
+            total++;
+        }
+
+        if (i === nums.length - 2 && nums[i] === nums[i + 1]) {
+            total++;
+        }
+    }
+
+    return total;
+};
+
+console.log(countClumps([1, 2, 2, 3, 4, 4]));
+console.log(countClumps([1, 1, 2, 1, 1]));
+console.log(countClumps([1, 1, 1, 1, 1]));
