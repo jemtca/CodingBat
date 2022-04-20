f314609556a8d99c2cc05287141ba38b11f5a7bf
jemtca <30645648+jemtca@users.noreply.github.com>
2022-04-19 22:08:49 -0700

    CodingBat

--- JavaScript/AP-1/scoresIncreasing.js
+++ JavaScript/AP-1/scoresIncreasing.js
@@ -1,2 +1,16 @@
 
-// 
+// given an array of scores, return true if each score is equal or greater than the one before
+// the array will be length 2 or more
+const scoresIncreasing = (scores) => {
+    for (let i = 1; i < scores.length; i++) {
+        if (scores[i] < scores[i - 1]) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+console.log(scoresIncreasing([1, 3, 4]));
+console.log(scoresIncreasing([1, 3, 2]));
+console.log(scoresIncreasing([1, 1, 4]));
