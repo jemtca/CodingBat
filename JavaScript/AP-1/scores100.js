4376163ccdee8bf925ce9666b091a433e8011427
jemtca <30645648+jemtca@users.noreply.github.com>
2022-04-20 19:23:22 -0700

    CodingBat

--- JavaScript/AP-1/scores100.js
+++ JavaScript/AP-1/scores100.js
@@ -1,2 +1,16 @@
 
-// 
+// given an array of scores, return true if there are scores of 100 next to each other in the array
+// the array length will be at least 2
+const scores100 = (scores) => {
+    for (let i = 0; i < scores.length - 1; i++) {
+        if (scores[i] === 100 && scores[i + 1] === 100) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+console.log(scores100([1, 100, 100]));
+console.log(scores100([1, 100, 99, 100]));
+console.log(scores100([100, 1, 100, 100]));
