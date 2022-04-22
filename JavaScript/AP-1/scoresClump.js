7eba24f7490131ff72e91ad834091bdc167e8d9d
jemtca <30645648+jemtca@users.noreply.github.com>
2022-04-21 22:16:07 -0700

    CodingBat

--- JavaScript/AP-1/scoresClump.js
+++ JavaScript/AP-1/scoresClump.js
@@ -1,2 +1,16 @@
 
-// 
+// given an array of scores sorted in increasing order, return true if the array contains 3 adjacent scores that differ from each other by at most 2
+// such as with {3, 4, 5} or {3, 5, 5}
+const scoresClump = (scores) => {
+    for (let i = 0; i < scores.length - 2; i++) {
+        if (Math.abs(scores[i] - scores[i + 1]) <= 2 && Math.abs(scores[i + 1] - scores[i + 2]) <= 2 && Math.abs(scores[i] - scores[i + 2]) <= 2) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+console.log(scoresClump([3, 4, 5]));
+console.log(scoresClump([3, 4, 6]));
+console.log(scoresClump([1, 3, 5, 5]));
