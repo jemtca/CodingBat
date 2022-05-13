f22be8ec859f72145ea19954849e6c55ef2c4bc2
jemtca <30645648+jemtca@users.noreply.github.com>
2022-05-12 21:38:51 -0700

    CodingBat

--- JavaScript/AP-1/wordsCount.js
+++ JavaScript/AP-1/wordsCount.js
@@ -1,2 +1,17 @@
 
-// 
+// given an array of strings, return the count of the number of strings with the given length
+const wordsCount = (words, len) => {
+    let result = 0;
+
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length === len) {
+            result++;
+        }
+    }
+
+    return result;
+};
+
+console.log(wordsCount(['a', 'bb', 'b', 'ccc'], 1));
+console.log(wordsCount(['a', 'bb', 'b', 'ccc'], 3));
+console.log(wordsCount(['a', 'bb', 'b', 'ccc'], 4));
