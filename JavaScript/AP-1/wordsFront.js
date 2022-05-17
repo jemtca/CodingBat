3c519d054082750acabf3b1b62ebdac8c8255367
jemtca <30645648+jemtca@users.noreply.github.com>
2022-05-16 18:47:04 -0700

    CodingBat

--- JavaScript/AP-1/wordsFront.js
+++ JavaScript/AP-1/wordsFront.js
@@ -1,2 +1,16 @@
 
-// 
+// given an array of strings, return a new array containing the first n strings
+// n will be in the range 1..length
+const wordsFront = (words, n) => {
+    let newArray = [];
+
+    for (let i = 0; i < n; i++) {
+        newArray.push(words[i]);
+    }
+
+    return newArray;
+};
+
+console.log(wordsFront(['a', 'b', 'c', 'd'], 1));
+console.log(wordsFront(['a', 'b', 'c', 'd'], 2));
+console.log(wordsFront(['a', 'b', 'c', 'd'], 3));
