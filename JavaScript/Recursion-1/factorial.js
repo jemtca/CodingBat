3a0b1f7c2ee805c41738541876b4e480bb323db2
jemtca <30645648+jemtca@users.noreply.github.com>
2022-09-13 21:43:38 -0700

    CodingBat

--- JavaScript/Recursion-1/factorial.js
+++ JavaScript/Recursion-1/factorial.js
@@ -1,2 +1,13 @@
 
-// 
+// given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1
+const factorial = (n) => {
+    if (n === 1) {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+};
+
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(3));
