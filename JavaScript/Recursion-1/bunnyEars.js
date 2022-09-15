3d16e84b55a0ca72c62f2a230043e3c68152ae32
jemtca <30645648+jemtca@users.noreply.github.com>
2022-09-14 23:54:21 -0700

    CodingBat

--- JavaScript/Recursion-1/bunnyEars.js
+++ JavaScript/Recursion-1/bunnyEars.js
@@ -1,2 +1,14 @@
 
-// 
+// we have a number of bunnies and each bunny has two big floppy ears
+// we want to compute the total number of ears across all the bunnies recursively
+const bunnyEars = (bunnies) => {
+	if (bunnies === 0) {
+		return 0;
+	}
+
+	return 2 + bunnyEars(bunnies - 1);
+};
+
+console.log(bunnyEars(0));
+console.log(bunnyEars(1));
+console.log(bunnyEars(2));
