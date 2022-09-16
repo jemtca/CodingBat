a1de3ca6095a63f00c92837da79ed1c92c7a4cde
jemtca <30645648+jemtca@users.noreply.github.com>
2022-09-15 23:08:13 -0700

    CodingBat

--- JavaScript/Recursion-1/fibonacci.js
+++ JavaScript/Recursion-1/fibonacci.js
@@ -1,2 +1,16 @@
 
-// 
+// the fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition
+// the first two values in the sequence are 0 and 1 (essentially 2 base cases)
+// each subsequent value is the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on
+// define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence
+const fibonacci = (n) => {
+	if (n < 2) {
+		return n;
+	}
+
+	return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+console.log(fibonacci(0));
+console.log(fibonacci(1));
+console.log(fibonacci(2));
