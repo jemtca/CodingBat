d59a646a85fa25d23d408e49d65a00efbdffece4
jemtca <30645648+jemtca@users.noreply.github.com>
2022-09-19 20:27:32 -0700

    CodingBat

--- JavaScript/Recursion-1/factorial.js
+++ JavaScript/Recursion-1/factorial.js
@@ -1,5 +1,6 @@
 
 // given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1
+// compute the result recursively
 const factorial = (n) => {
     if (n === 1) {
         return 1;
--- JavaScript/Recursion-1/triangle.js
+++ JavaScript/Recursion-1/triangle.js
@@ -1,2 +1,15 @@
 
-// 
+// we have triangle made of blocks
+// the topmost row has 1 block, the next row down has 2 blocks, the next row has 3 blocks, and so on
+// compute recursively the total number of blocks in such a triangle with the given number of rows
+const triangle = (rows) => {
+	if (rows === 0) {
+		return 0;
+	} 
+
+	return rows + triangle(rows - 1);
+};
+
+console.log(triangle(0));
+console.log(triangle(1));
+console.log(triangle(2));
