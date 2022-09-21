25b4b6da8208cc0730fa3a61d6e49386587b5a6c
jemtca <30645648+jemtca@users.noreply.github.com>
2022-09-20 20:50:36 -0700

    CodingBat

--- JavaScript/Recursion-1/sumDigits.js
+++ JavaScript/Recursion-1/sumDigits.js
@@ -1,2 +1,15 @@
 
-// 
+// given a non-negative int n, return the sum of its digits recursively
+// note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6)
+// while divide (/) by 10 removes the rightmost digit (126 / 10 is 12)
+const sumDigits = (n) => {
+    if (n === 0) {
+        return 0;
+    }
+
+    return (n % 10) + sumDigits(Math.floor(n / 10));
+};
+
+console.log(sumDigits(126));
+console.log(sumDigits(49));
+console.log(sumDigits(12));
