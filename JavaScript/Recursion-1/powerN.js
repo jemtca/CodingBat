13d2e048e16a3e2d75226b15ec58521a99d5ffd2
jemtca <30645648+jemtca@users.noreply.github.com>
2022-09-23 21:58:11 -0700

    CodingBat

--- JavaScript/Recursion-1/powerN.js
+++ JavaScript/Recursion-1/powerN.js
@@ -1,2 +1,13 @@
 
-// 
+// given base and n that are both 1 or more, compute recursively the value of base to the n power, so powerN(3, 2) is 9 (3 squared)
+const powerN = (base, n) => {
+    if (n === 0) {
+        return 1;
+    }
+
+    return base * powerN(base, n - 1);
+};
+
+console.log(powerN(3, 1));
+console.log(powerN(3, 2));
+console.log(powerN(3, 3));
