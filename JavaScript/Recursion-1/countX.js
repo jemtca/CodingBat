ffd3ffb4b55e577aa6a93d4874eddce91be4b8f9
jemtca <30645648+jemtca@users.noreply.github.com>
2022-09-26 21:34:41 -0700

    CodingBat

--- JavaScript/Recursion-1/countX.js
+++ JavaScript/Recursion-1/countX.js
@@ -1,2 +1,13 @@
 
-// 
+// given a string, compute recursively the number of lowercase 'x' chars in the string
+const countX = (str) => {
+    if (!str) {
+        return 0;
+    }
+
+    return str[0] === 'x' ? 1 + countX(str.substring(1)) : countX(str.substring(1));
+};
+
+console.log(countX('xxhixx'));
+console.log(countX('xhixhix'));
+console.log(countX('hi'));
