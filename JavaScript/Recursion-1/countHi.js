5516525e93cd7499b7ee691700808ad524c5b00b
jemtca <30645648+jemtca@users.noreply.github.com>
2022-09-27 21:00:21 -0700

    CodingBat

--- JavaScript/Recursion-1/countHi.js
+++ JavaScript/Recursion-1/countHi.js
@@ -1,2 +1,13 @@
 
-// 
+// given a string, compute recursively the number of times lowercase 'hi' appears in the string
+const countHi = (str) => {
+    if (!str) {
+        return 0;
+    }
+
+    return str.substring(0, 2) === 'hi' ? 1 + countHi(str.substring(2)) : countHi(str.substring(1));
+};
+
+console.log(countHi('xxhixx'));
+console.log(countHi('xhixhix'));
+console.log(countHi('hi'));
