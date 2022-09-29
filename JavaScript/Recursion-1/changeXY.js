95855d77af7c9ba4bdb307202705b7921e00ba6b
jemtca <30645648+jemtca@users.noreply.github.com>
2022-09-28 22:26:00 -0700

    CodingBat

--- JavaScript/Recursion-1/changeXY.js
+++ JavaScript/Recursion-1/changeXY.js
@@ -1,2 +1,13 @@
 
-// 
+// given a string, compute recursively a new string where all the lowercase 'x' chars have been changed to 'y' chars
+const changeXY = (str) => {
+    if (!str) {
+        return '';
+    }
+
+    return str[0] === 'x' ? 'y' + changeXY(str.substring(1)) : str[0] + changeXY(str.substring(1));
+};
+
+console.log(changeXY('codex'));
+console.log(changeXY('xxhixx'));
+console.log(changeXY('xhixhix'));
