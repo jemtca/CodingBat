27cac6f38421f41ff51c7e95e50b0c6ca4897d20
jemtca <30645648+jemtca@users.noreply.github.com>
2022-09-29 22:24:46 -0700

    CodingBat

--- JavaScript/Recursion-1/changePi.js
+++ JavaScript/Recursion-1/changePi.js
@@ -1,2 +1,13 @@
 
-// 
+// given a string, compute recursively a new string where all appearances of 'pi' have been replaced by '3.14'
+const changePi = (str) => {
+    if (!str) {
+        return '';
+    }
+
+    return str.substring(0, 2) === 'pi' ? '3.14' + changePi(str.substring(2)) : str[0] + changePi(str.substring(1));
+};
+
+console.log(changePi('xpix'));
+console.log(changePi('pipi'));
+console.log(changePi('pip'));
