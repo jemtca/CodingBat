fba9fb934dcdb7ddcba56112430df86e0677de3b
jemtca <30645648+jemtca@users.noreply.github.com>
2022-09-30 17:27:18 -0700

    CodingBat

--- JavaScript/Recursion-1/noX.js
+++ JavaScript/Recursion-1/noX.js
@@ -1,2 +1,13 @@
 
-// 
+// given a string, compute recursively a new string where all the 'x' chars have been removed
+const noX = (str) => {
+    if (!str) {
+        return '';
+    }
+
+    return str[0] !== 'x' ? str[0] + noX(str.substring(1)) : noX(str.substring(1));
+};
+
+console.log(noX('xaxb'));
+console.log(noX('abc'));
+console.log(noX('xx'));
