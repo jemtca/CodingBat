c02c5ca21847136ce9f5030cb880d07006eebd76
jemtca <30645648+jemtca@users.noreply.github.com>
2022-10-05 16:07:52 -0700

    CodingBat

--- JavaScript/Recursion-1/array220.js
+++ JavaScript/Recursion-1/array220.js
@@ -1,2 +1,16 @@
 
-// 
+// given an array of ints, compute recursively if the array contains somewhere a value followed in the array by that value times 10
+// we'll use the convention of considering only the part of the array that begins at the given index
+// in this way, a recursive call can pass index+1 to move down the array
+// the initial call will pass in index as 0
+const array220 = (nums, index) => {
+    if (nums.length < 1 || index === nums.length - 1) {
+        return false;
+    }
+
+    return nums[index] * 10 === nums[index + 1] ? true : array220(nums, index + 1);
+};
+
+console.log(array220([1, 2, 20], 0));
+console.log(array220([3, 30], 0));
+console.log(array220([3], 0));
