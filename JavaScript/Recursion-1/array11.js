7d263ef0eba2ad6e99c72fa180247ae914f093d7
jemtca <30645648+jemtca@users.noreply.github.com>
2022-10-04 18:57:43 -0700

    CodingBat

--- JavaScript/Recursion-1/array11.js
+++ JavaScript/Recursion-1/array11.js
@@ -1,2 +1,16 @@
 
-// 
+// given an array of ints, compute recursively the number of times that the value 11 appears in the array
+// we'll use the convention of considering only the part of the array that begins at the given index
+// in this way, a recursive call can pass index+1 to move down the array
+// the initial call will pass in index as 0
+const array11 = (nums, index) => {
+    if (index === nums.length) {
+        return 0;
+    }
+
+    return nums[index] === 11 ? 1 + array11(nums, index + 1) : array11(nums, index + 1);
+};
+
+console.log(array11([1, 2, 11], 0));
+console.log(array11([11, 11], 0));
+console.log(array11([1, 2, 3, 4], 0));
