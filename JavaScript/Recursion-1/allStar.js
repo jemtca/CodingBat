ea74a1e5e20290e77f75cfc29647ee5ee158314e
jemtca <30645648+jemtca@users.noreply.github.com>
2022-10-06 19:10:37 -0700

    CodingBat

--- JavaScript/Recursion-1/allStar.js
+++ JavaScript/Recursion-1/allStar.js
@@ -1,2 +1,13 @@
 
-// 
+// given a string, compute recursively a new string where all the adjacent chars are now separated by a '*'
+const allStar = (str) => {
+    if (!str) {
+        return '';
+    }
+
+    return str.length > 1 ? str[0] + '*' + allStar(str.substring(1)) : str[0] + allStar(str.substring(1));
+};
+
+console.log(allStar('hello'));
+console.log(allStar('abc'));
+console.log(allStar('ab'));
