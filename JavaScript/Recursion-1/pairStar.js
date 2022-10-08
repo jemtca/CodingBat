20f25dbabacc2ca05f4b04a3d5b6825eb2db6eb5
jemtca <30645648+jemtca@users.noreply.github.com>
2022-10-07 19:33:18 -0700

    CodingBat

--- JavaScript/Recursion-1/pairStar.js
+++ JavaScript/Recursion-1/pairStar.js
@@ -1,2 +1,13 @@
 
-// 
+// given a string, compute recursively a new string where identical chars that are adjacent in the original string are separated from each other by a '*'
+const pairStar = (str) => {
+    if (!str) {
+        return '';
+    }
+
+    return str[0] === str[1] ? str[0] + '*' + pairStar(str.substring(1)) : str[0] + pairStar(str.substring(1));
+};
+
+console.log(pairStar('hello'));
+console.log(pairStar('xxyy'));
+console.log(pairStar('aaaa'));
