5f781031fd49229651c9b48149041f74393a308e
jemtca <30645648+jemtca@users.noreply.github.com>
2022-10-14 14:41:52 -0700

    CodingBat

--- JavaScript/Recursion-1/endX.js
+++ JavaScript/Recursion-1/endX.js
@@ -1,2 +1,13 @@
 
-// 
+// given a string, compute recursively a new string where all the lowercase 'x' chars have been moved to the end of the string
+const endX = (str) => {
+    if (!str) {
+        return '';
+    }
+
+    return str[0] === 'x' ? endX(str.substring(1)) + 'x' : str[0] + endX(str.substring(1));
+};
+
+console.log(endX('xxre'));
+console.log(endX('xxhixx'));
+console.log(endX('xhixhix'));
