436889ebc73b6e9cc16ae83bc18d394c74cdc0be
jemtca <30645648+jemtca@users.noreply.github.com>
2022-10-24 13:20:39 -0700

    CodingBat

--- JavaScript/Recursion-1/countAbc.js
+++ JavaScript/Recursion-1/countAbc.js
@@ -1,2 +1,14 @@
 
-// 
+// count recursively the total number of 'abc' and 'aba' substrings that appear in the given string
+const countAbc = (str) => {
+	if (!str) {
+		return 0;
+	}
+
+	return (str.length > 2) && (str.substring(0, 3).toLowerCase() === 'abc' || str.substring(0, 3).toLowerCase() === 'aba')
+	? 1 + countAbc(str.substring(2)) : countAbc(str.substring(1));
+};
+
+console.log(countAbc('abc'));
+console.log(countAbc('abcxxabc'));
+console.log(countAbc('abaxxaba'));
