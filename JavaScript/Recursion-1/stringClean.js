0acb237f07de3c53ce2b27001d8a83bf2c81955d
jemtca <30645648+jemtca@users.noreply.github.com>
2022-10-26 15:06:09 -0700

    CodingBat

--- JavaScript/Recursion-1/stringClean.js
+++ JavaScript/Recursion-1/stringClean.js
@@ -1,2 +1,14 @@
 
-// 
+// given a string, return recursively a 'cleaned' string where adjacent chars that are the same have been reduced to a single char
+// so 'yyzzza' yields 'yza'
+const stringClean = (str) => {
+    if (!str) {
+        return '';
+    }
+
+    return str.length > 1 && str[0] === str[1] ? stringClean(str.substring(1)) : str[0] + stringClean(str.substring(1));
+};
+
+console.log(stringClean('yyzzza'));
+console.log(stringClean('abbbcdd'));
+console.log(stringClean('Hello'));
