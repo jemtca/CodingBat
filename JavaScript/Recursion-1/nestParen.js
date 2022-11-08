fbb376caa3bdaef7cc32ef9aa7e44c62c5b19629
jemtca <30645648+jemtca@users.noreply.github.com>
2022-11-08 15:05:02 -0800

    CodingBat

--- JavaScript/Recursion-1/nestParen.js
+++ JavaScript/Recursion-1/nestParen.js
@@ -1,2 +1,18 @@
 
-// 
+// given a string, return true if it is a nesting of zero or more pairs of parenthesis, like '(())' or '((()))'
+// suggestion: check the first and last chars, and then recur on what's inside them
+const nestParen = (str) => {
+	if (!str) {
+		return true;
+	}
+
+	if (str[0] === '(' && str[str.length - 1] === ')') {
+		return nestParen(str.substring(1, str.length - 1));
+	}
+
+	return false;
+};
+
+console.log(nestParen('(())'));
+console.log(nestParen('((()))'));
+console.log(nestParen('(((x))'));
