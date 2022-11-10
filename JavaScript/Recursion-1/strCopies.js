a5bc995eac95e9719571a4e4ffa84732285ec45c
jemtca <30645648+jemtca@users.noreply.github.com>
2022-11-10 15:05:34 -0800

    CodingBat

--- JavaScript/Recursion-1/strCopies.js
+++ JavaScript/Recursion-1/strCopies.js
@@ -1,2 +1,23 @@
 
-// 
+// given a string and a non-empty substring sub
+// compute recursively if at least n copies of sub appear in the string somewhere, possibly with overlapping
+// n will be non-negative
+const strCopies = (str, sub, n) => {
+    if (n === 0) {
+        return true;
+    }
+
+    if (str.length >= sub.length && str.substring(0, sub.length) === sub) {
+        return strCopies(str.substring(1), sub, n - 1);
+    }
+
+    if (str.length >= sub.length && str.substring(0, sub.length) !== sub) {
+        return strCopies(str.substring(1), sub, n);
+    }
+
+    return false;
+};
+
+console.log(strCopies('catcowcat', 'cat', 2));
+console.log(strCopies('catcowcat', 'cow', 2));
+console.log(strCopies('catcowcat', 'cow', 1));
