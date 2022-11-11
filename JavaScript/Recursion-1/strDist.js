6e7668e574a40f78c2aebec59a3c2cb6c5af259a
jemtca <30645648+jemtca@users.noreply.github.com>
2022-11-11 15:19:13 -0800

    CodingBat

--- JavaScript/Recursion-1/strDist.js
+++ JavaScript/Recursion-1/strDist.js
@@ -1,2 +1,22 @@
 
-// 
+// given a string and a non-empty substring sub
+// compute recursively the largest substring which starts and ends with sub and return its length
+const strDist = (str, sub) => {
+    if (!str || (str.length === sub.length && str !== sub)) {
+        return 0;
+    }
+
+    if (str.length >= sub.length && str.substring(0, sub.length) !== sub) {
+        return strDist(str.substring(1), sub);
+    }
+
+    if (str.length >= sub.length && str.substring(str.length - sub.length) !== sub) {
+        return strDist(str.substring(0, str.length - 1), sub);
+    }
+
+    return str.length;
+};
+
+console.log(strDist('catcowcat', 'cat'));
+console.log(strDist('catcowcat', 'cow'));
+console.log(strDist('cccatcowcatxx', 'cat'));
