e2c5e816db6086d8ef9832aff027412c10ce5acf
jemtca <30645648+jemtca@users.noreply.github.com>
2022-12-07 13:59:20 -0800

    CodingBat

--- JavaScript/Recursion-2/groupSum6.js
+++ JavaScript/Recursion-2/groupSum6.js
@@ -1,2 +1,20 @@
 
-// 
+// given an array of ints, is it possible to choose a group of some of the ints,
+// beginning at the start index, such that the group sums to the given target?
+// however, with the additional constraint that all 6's must be chosen
+const groupSum6 = (start, nums, target) => {
+	if (start >= nums.length) {
+		return target === 0;
+	} else {
+		if (nums[start] === 6) {
+			return groupSum6(start + 1, nums, target - nums[start]);
+		} else {
+			return groupSum6(start + 1, nums, target - nums[start]) || groupSum6(start + 1, nums, target);
+		}
+	}
+};
+
+
+console.log(groupSum6(0, [5, 6, 2], 8));
+console.log(groupSum6(0, [5, 6, 2], 9));
+console.log(groupSum6(0, [5, 6, 2], 7));
