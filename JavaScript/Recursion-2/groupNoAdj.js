816951373159442039c0d04657b234eec8845957
jemtca <30645648+jemtca@users.noreply.github.com>
2022-12-08 15:39:29 -0800

    CodingBat

--- JavaScript/Recursion-2/groupNoAdj.js
+++ JavaScript/Recursion-2/groupNoAdj.js
@@ -1,2 +1,15 @@
 
-// 
+// given an array of ints, is it possible to choose a group of some of the ints,
+// such that the group sums to the given target with this additional constraint:
+// if a value in the array is chosen to be in the group, the value immediately following it in the array must not be chosen
+const groupNoAdj = (start, nums, target) => {
+    if (start >= nums.length) {
+        return (target === 0);
+    } else {
+        return groupNoAdj(start + 2, nums, target - nums[start]) || groupNoAdj(start + 1, nums, target);
+    }
+};
+
+console.log(groupNoAdj(0, [2, 5, 10, 4], 12));
+console.log(groupNoAdj(0, [2, 5, 10, 4], 14));
+console.log(groupNoAdj(0, [2, 5, 10, 4], 7));
