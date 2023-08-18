ba64f90784c9857a082316c1f02c9b4af7ba492f
jemtca <30645648+jemtca@users.noreply.github.com>
2023-08-18 13:08:30 -0700

    CodingBat

--- JavaScript/Map-1/mapBully.js
+++ JavaScript/Map-1/mapBully.js
@@ -1,2 +1,17 @@
 
-// 
+// modify and return the given map as follows: if the key 'a' has a value, set the key 'b' to have that value, and set the key 'a' to have the value ''
+// basically 'b' is a bully, taking the value and replacing it with the empty string
+const mapBully = (map) => {
+    const myMap = new Map(Object.entries(map));
+
+    if (myMap.has('a')) {
+        myMap.set('b', myMap.get('a'));
+        myMap.set('a', '');
+    }
+
+    return myMap;
+};
+
+console.log(mapBully({'a': 'candy', 'b': 'dirt'}));
+console.log(mapBully({'a': 'candy'}));
+console.log(mapBully({'a': 'candy', 'b': 'carrot', 'c': 'meh'}));
