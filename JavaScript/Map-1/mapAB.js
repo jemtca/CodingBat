cdc831e35fa318fdab9e4ef2f6a16f4129c1372d
jemtca <30645648+jemtca@users.noreply.github.com>
2023-12-12 22:01:36 -0800

    CodingBat

--- JavaScript/Map-1/mapAB.js
+++ JavaScript/Map-1/mapAB.js
@@ -1,2 +1,16 @@
 
-// 
+// modify and return the given map as follows: for this problem the map may or may not contain the 'a' and 'b' keys
+// if both keys are present, append their 2 string values together and store the result under the key 'ab'
+const mapAB = (map) => {
+    const myMap = new Map(Object.entries(map));
+
+    if (myMap.has('a') && myMap.has('b')) {
+        myMap.set('ab', myMap.get('a') + myMap.get('b'));
+    }
+
+    return myMap;
+};
+
+console.log(mapAB({'a': 'Hi', 'b': 'There'}));
+console.log(mapAB({'a': 'Hi'}));
+console.log(mapAB({'b': 'There'}));
