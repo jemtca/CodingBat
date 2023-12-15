d7745dd716f8d72c5689ecb2485c10524e9973de
jemtca <30645648+jemtca@users.noreply.github.com>
2023-12-14 22:37:30 -0800

    CodingBat

--- JavaScript/Map-1/topping2.js
+++ JavaScript/Map-1/topping2.js
@@ -1,2 +1,21 @@
 
-// 
+// given a map of food keys and their topping values, modify and return the map as follows:
+// if the key 'ice cream' has a value, set that as the value for the key 'yogurt' also
+// if the key 'spinach' has a value, change that value to 'nuts'
+const topping2 = (map) => {
+    const myMap = new Map(Object.entries(map));
+
+    if (myMap.has('ice cream')) {
+        myMap.set('yogurt', myMap.get('ice cream'));
+    }
+
+    if (myMap.has('spinach')) {
+        myMap.set('spinach', 'nuts');
+    }
+
+    return myMap;
+};
+
+console.log(topping2({'ice cream': 'cherry'}));
+console.log(topping2({'spinach': 'dirt', 'ice cream': 'cherry'}));
+console.log(topping2({'yogurt': 'salt'}));
