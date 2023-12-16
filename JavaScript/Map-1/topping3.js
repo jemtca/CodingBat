6a33aacb4608e9d1a50d881791df0075d9e3d6fd
jemtca <30645648+jemtca@users.noreply.github.com>
2023-12-15 23:53:35 -0800

    CodingBat

--- JavaScript/Map-1/topping3.js
+++ JavaScript/Map-1/topping3.js
@@ -1,2 +1,21 @@
 
-// 
+// given a map of food keys and topping values, modify and return the map as follows:
+// if the key 'potato' has a value, set that as the value for the key 'fries'
+// if the key 'salad' has a value, set that as the value for the key 'spinach'
+const topping3 = (map) => {
+    const myMap = new Map(Object.entries(map));
+
+    if (myMap.has('potato')) {
+        myMap.set('fries', myMap.get('potato'));
+    }
+
+    if (myMap.has('salad')) {
+        myMap.set('spinach', myMap.get('salad'));
+    }
+
+    return myMap;
+};
+
+console.log(topping3({'potato': 'ketchup'}));
+console.log(topping3({'potato': 'butter'}));
+console.log(topping3({'salad': 'oil', 'potato': 'ketchup'}));
