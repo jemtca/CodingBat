0e0a407cbfb296f9d72a943fd118f0e8218c81fb
jemtca <30645648+jemtca@users.noreply.github.com>
2023-12-18 23:10:41 -0800

    CodingBat

--- JavaScript/Map-1/mapAB2.js
+++ JavaScript/Map-1/mapAB2.js
@@ -1,2 +1,16 @@
 
-// 
+// modify and return the given map as follows: if the keys 'a' and 'b' are both in the map and have equal values, remove them both
+const mapAB2 = (map) => {
+    const myMap = new Map(Object.entries(map));
+
+    if ((myMap.has('a') && myMap.has('b')) && (myMap.get('a') === myMap.get('b'))) {
+        myMap.delete('a');
+        myMap.delete('b');
+    }
+
+    return myMap;
+};
+
+console.log(mapAB2({'a': 'aaa', 'b': 'aaa', 'c': 'cake'}));
+console.log(mapAB2({'a': 'aaa', 'b': 'bbb'}));
+console.log(mapAB2({'a': 'aaa', 'b': 'bbb', 'c': 'aaa'}));
