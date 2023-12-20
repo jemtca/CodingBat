45c26ec639fbcb5781724864abc8b8894cff107b
jemtca <30645648+jemtca@users.noreply.github.com>
2023-12-19 23:51:58 -0800

    CodingBat

--- JavaScript/Map-1/mapAB3.js
+++ JavaScript/Map-1/mapAB3.js
@@ -1,2 +1,20 @@
 
-// 
+// modify and return the given map as follows:
+// if exactly one of the keys 'a' or 'b' has a value in the map (but not both), set the other to have that same value in the map
+const mapAB3 = (map) => {
+    const myMap = new Map(Object.entries(map));
+    
+    if (myMap.has('a') && !myMap.has('b')) {
+        myMap.set('b', myMap.get('a'));
+    }
+
+    if (!myMap.has('a') && myMap.has('b')) {
+        myMap.set('a', myMap.get('b'));
+    }
+
+    return myMap;
+};
+
+console.log(mapAB3({'a': 'aaa', 'c': 'cake'}));
+console.log(mapAB3({'b': 'bbb', 'c': 'cake'}));
+console.log(mapAB3({'a': 'aaa', 'b': 'bbb', 'c': 'cake'}));
