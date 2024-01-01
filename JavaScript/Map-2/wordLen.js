d968dfa8eab328b0964ba015a434e79b6ac1661e
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-01 15:03:08 -0800

    CodingBat

--- JavaScript/Map-2/wordLen.js
+++ JavaScript/Map-2/wordLen.js
@@ -1,2 +1,17 @@
 
-// 
+// given an array of strings, return a Map<String, Integer> containing a key for every different string in the array, and the value is that string's length
+const wordLen = (strings) => {
+    const myMap = new Map();
+
+    for (let i = 0; i < strings.length; i++) {
+        if (!myMap.has(strings[i])) {
+            myMap.set(strings[i], strings[i].length);
+        }
+    }
+
+    return myMap;
+};
+
+console.log(wordLen(['a', 'bb', 'a', 'bb']));
+console.log(wordLen(['this', 'and', 'that', 'and']));
+console.log(wordLen(['code', 'code', 'code', 'bug']));
