8f94c0905bacd59d0f80d19228461426d23c0b06
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-02 20:24:14 -0800

    CodingBat

--- JavaScript/Map-2/pairs.js
+++ JavaScript/Map-2/pairs.js
@@ -1,2 +1,16 @@
 
-// 
+// given an array of non-empty strings, create and return a Map<String, String> as follows:
+// for each string add its first character as a key with its last character as the value
+const pairs = (strings) => {
+    const myMap = new Map();
+
+    for (let string of strings) {
+        myMap.set(string[0], string[string.length - 1]);
+    }
+
+    return myMap;
+};
+
+console.log(pairs(['code', 'bug']));
+console.log(pairs(['man', 'moon', 'main']));
+console.log(pairs(['man', 'moon', 'good', 'night']));
