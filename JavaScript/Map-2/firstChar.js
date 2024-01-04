655758e856e1b5a98b942ffbf1d35621a23dac37
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-04 13:21:56 -0800

    CodingBat

--- JavaScript/Map-2/firstChar.js
+++ JavaScript/Map-2/firstChar.js
@@ -1,2 +1,23 @@
 
-// 
+// given an array of non-empty strings
+// return a Map<String, String> with a key for every different first character seen
+// with the value of all the strings starting with that character appended together in the order they appear in the array
+const firstChar = (strings) => {
+    const myMap = new Map();
+
+    for (let i = 0; i < strings.length; i++) {
+        if (!myMap.has(strings[i][0])) {
+            myMap.set(strings[i][0], strings[i]);
+        } else {
+            const prevStr = myMap.get(strings[i][0]);
+
+            myMap.set(strings[i][0], prevStr + strings[i]);
+        }
+    }
+
+    return myMap;
+};
+
+console.log(firstChar(['salt', 'tea', 'soda', 'toast']));
+console.log(firstChar(['aa', 'bb', 'cc', 'aAA', 'cCC', 'd']));
+console.log(firstChar([]));
