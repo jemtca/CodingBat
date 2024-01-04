4423ebee3d469179a4f647d40cb3284e525cffcb
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-03 22:22:45 -0800

    CodingBat

--- JavaScript/Map-2/wordCount.js
+++ JavaScript/Map-2/wordCount.js
@@ -1,2 +1,20 @@
 
-// 
+// the classic word-count algorithm: given an array of strings
+// return a Map<String, Integer> with a key for each different string, with the value the number of times that string appears in the array
+const wordCount = (strings) => {
+    const myMap = new Map();
+
+    for (let i = 0; i < strings.length; i++) {
+        if (!myMap.has(strings[i])) {
+            myMap.set(strings[i], 1);
+        } else {
+            myMap.set(strings[i], myMap.get(strings[i]) + 1);
+        }
+    }
+
+    return myMap;
+};
+
+console.log(wordCount(['a', 'b', 'a', 'c', 'b']));
+console.log(wordCount(['c', 'b', 'a']));
+console.log(wordCount(['c', 'c', 'c', 'c']));
