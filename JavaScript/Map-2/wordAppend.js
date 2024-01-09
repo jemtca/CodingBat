2d5888a182c6bfa42a3f3c6aceef9b8df8d0be80
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-08 22:09:49 -0800

    CodingBat

--- JavaScript/Map-2/wordAppend.js
+++ JavaScript/Map-2/wordAppend.js
@@ -1,2 +1,26 @@
 
-// 
+// loop over the given array of strings to build a result string like this:
+// when a string appears the 2nd, 4th, 6th, etc. time in the array, append the string to the result
+// return the empty string if no string appears a 2nd time
+const wordAppend = (strings) => {
+    const myMap = new Map();
+    let str = '';
+
+    for (let i = 0; i < strings.length; i++) {
+        if (!myMap.has(strings[i])) {
+            myMap.set(strings[i], 1);
+        } else {
+            myMap.set(strings[i], myMap.get(strings[i]) + 1);
+        }
+        
+        if (myMap.get(strings[i]) % 2 === 0) {
+            str = str + strings[i];
+        }
+    }
+
+    return str;
+};
+
+console.log(wordAppend(['a', 'b', 'a']));
+console.log(wordAppend(['a', 'b', 'a', 'c', 'a', 'd', 'a']));
+console.log(wordAppend(['a', '', 'a']));
