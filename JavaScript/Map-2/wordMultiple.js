04601902e4808474ef49c60fd4587b2c4f89a9e8
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-09 21:46:59 -0800

    CodingBat

--- JavaScript/Map-2/wordMultiple.js
+++ JavaScript/Map-2/wordMultiple.js
@@ -1,2 +1,20 @@
 
-// 
+// Given an array of strings
+// return a Map<String, Boolean> where each different string is a key and its value is true if that string appears 2 or more times in the array
+const wordMultiple = (strings) => {
+    const myMap = new Map();
+
+    for (let i = 0; i < strings.length; i++) {
+        if (!myMap.has(strings[i])) {
+            myMap.set(strings[i], false);
+        } else {
+            myMap.set(strings[i], true);
+        }
+    }
+
+    return myMap;
+};
+
+console.log(wordMultiple(['a', 'b', 'a', 'c', 'b']));
+console.log(wordMultiple(['c', 'b', 'a']));
+console.log(wordMultiple(['c', 'c', 'c', 'c']));
