212de988d5a9208515a82fd06a931441bddfab91
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-12 21:34:24 -0800

    CodingBat

--- JavaScript/Functional-1/doubling.js
+++ JavaScript/Functional-1/doubling.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of integers, return a list where each integer is multiplied by 2
+const doubling = (nums) => {
+    const newList = nums.map(n => n * 2);
+
+    return newList;
+};
+
+console.log(doubling([1, 2, 3]));
+console.log(doubling([6, 8, 6, 8, -1]));
+console.log(doubling([]));
