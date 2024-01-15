1c84be649da44e157f11c92ee91569036a799073
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-14 22:40:21 -0800

    CodingBat

--- JavaScript/Functional-1/square.js
+++ JavaScript/Functional-1/square.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of integers, return a list where each integer is multiplied with itself
+const square = (nums) => {
+    const newList = nums.map(n => n * n);
+
+    return newList;
+};
+
+console.log(square([1, 2, 3]));
+console.log(square([6, 8, -6, -8, 1]));
+console.log(square([]));
