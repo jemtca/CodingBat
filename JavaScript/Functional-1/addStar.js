513af011cb3df57fc6de46c0be2c31b73a7f7ab7
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-15 18:20:29 -0800

    CodingBat

--- JavaScript/Functional-1/addStar.js
+++ JavaScript/Functional-1/addStar.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of strings, return a list where each string has '*' added at its end
+const addStar = (strings) => {
+    const newList = strings.map(n => n + '*');
+
+    return newList;
+};
+
+console.log(addStar(['a', 'bb', 'ccc']));
+console.log(addStar(['hello', 'there']));
+console.log(addStar(['*']));
