4c6b826a964a100adf239b31e94a930ec95cfa5e
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-16 23:06:34 -0800

    CodingBat

--- JavaScript/Functional-1/copies3.js
+++ JavaScript/Functional-1/copies3.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of strings, return a list where each string is replaced by 3 copies of the string concatenated together
+const copies3 = (strings) => {
+    const newList = strings.map(n => n + n + n);
+
+    return newList;
+};
+
+console.log(copies3(["a", "bb", "ccc"]));
+console.log(copies3(["24", "a", ""]));
+console.log(copies3(["hello", "there"]));
