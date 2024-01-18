b81b72d13b51c04c4bb7759b656cda367eba2510
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-17 17:32:21 -0800

    CodingBat

--- JavaScript/Functional-1/moreY.js
+++ JavaScript/Functional-1/moreY.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of strings, return a list where each string has 'y' added at its start and end
+const moreY = (strings) => {
+    const newList = strings.map(n => 'y' + n + 'y');
+
+    return newList;
+};
+
+console.log(moreY(['a', 'b', 'c']));
+console.log(moreY(['hello', 'there']));
+console.log(moreY(['yay']));
