6a9889343dd5af0b40ea5a212af639ca36132b68
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-18 21:52:42 -0800

    CodingBat

--- JavaScript/Functional-1/math1.js
+++ JavaScript/Functional-1/math1.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of integers, return a list where each integer is added to 1 and the result is multiplied by 10
+const math1 = (nums) => {
+    const newList= nums.map(n => (n + 1) * 10);
+
+    return newList;
+};
+
+console.log(math1([1, 2, 3]));
+console.log(math1([6, 8, 6, 8, 1]));
+console.log(math1([10]));
