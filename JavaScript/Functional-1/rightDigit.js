6a3b41ccd98fc6c37a083ca04d50e5d96a093d07
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-19 23:56:28 -0800

    CodingBat

--- JavaScript/Functional-1/rightDigit.js
+++ JavaScript/Functional-1/rightDigit.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of non-negative integers, return an integer list of the rightmost digits
+const rightDigit = (nums) => {
+    const newList = nums.map(n => n % 10);
+
+    return newList;
+};
+
+console.log(rightDigit([1, 22, 93]));
+console.log(rightDigit([16, 8, 886, 8, 1]));
+console.log(rightDigit([10, 0]));
