d52714a60b1d3cd4055b978f602e7cf4b3eb43b0
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-26 15:00:29 -0800

    CodingBat

--- JavaScript/Functional-1/lower.js
+++ JavaScript/Functional-1/lower.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of strings, return a list where each string is converted to lower case
+const lower = (strings) => {
+    const newList = strings.map(n => n.toLowerCase());
+
+    return newList;
+};
+
+console.log(lower(['Hello', 'Hi']));
+console.log(lower(['AAA', 'BBB', 'ccc']));
+console.log(lower(['KitteN', 'ChocolaTE']));
