e2c035851bf4db8290fd3ddb111b59b511db7819
jemtca <30645648+jemtca@users.noreply.github.com>
2024-01-27 14:35:36 -0800

    CodingBat

--- JavaScript/Functional-1/noX.js
+++ JavaScript/Functional-1/noX.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of strings, return a list where each string has all its 'x' removed
+const noX = (strings) => {
+    const newList = strings.map(n => n.replaceAll('x', ''));
+
+    return newList;
+};
+
+console.log(noX(['ax', 'bb', 'cx']));
+console.log(noX(['xxax', 'xbxbx', 'xxcx']));
+console.log(noX(['x']));
