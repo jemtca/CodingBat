c1d1ee62f896bfa11b7afd153d4114354afe38f2
jemtca <30645648+jemtca@users.noreply.github.com>
2024-06-28 12:54:39 -0700

    CodingBat

--- JavaScript/Functional-2/noNeg.js
+++ JavaScript/Functional-2/noNeg.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of integers, return a list of the integers, omitting any that are less than 0
+const noNeg = (nums) => {
+    const newList = nums.filter(n => n >= 0);
+
+    return newList;
+};
+
+console.log(noNeg([1, -2]));
+console.log(noNeg([-3, -3, 3, 3]));
+console.log(noNeg([-1, -1, -1]));
