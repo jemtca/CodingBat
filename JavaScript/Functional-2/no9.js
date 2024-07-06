c3ab282fb6d2685e54e501412d1e894146939026
jemtca <30645648+jemtca@users.noreply.github.com>
2024-07-05 20:39:53 -0700

    CodingBat

--- JavaScript/Functional-2/no9.js
+++ JavaScript/Functional-2/no9.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of non-negative integers, return a list of those numbers except omitting any that end with 9
+const no9 = (nums) => {
+    const newList = nums.filter(n => n % 10 !== 9);
+
+    return newList;
+};
+
+console.log(no9([1, 2, 19]));
+console.log(no9([9, 19, 29, 3]));
+console.log(no9([1, 2, 3]));
