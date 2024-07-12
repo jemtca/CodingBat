a9f87a653f46353c2f5cf6cd17fc556b5fd7be8a
jemtca <30645648+jemtca@users.noreply.github.com>
2024-07-12 11:14:06 -0700

    CodingBat

--- JavaScript/Functional-2/noTeen.js
+++ JavaScript/Functional-2/noTeen.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of integers, return a list of those numbers, omitting any that are between 13 and 19 inclusive
+const noTeen = (nums) => {
+    const newList = nums.filter(n => n < 13 || n > 19);
+
+    return newList;
+};
+
+console.log(noTeen([12, 13, 19, 20]));
+console.log(noTeen([1, 14, 1]));
+console.log(noTeen([15]));
