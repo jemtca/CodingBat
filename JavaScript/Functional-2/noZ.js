106059e38b54d3629193ff253ee9b713d50979c7
jemtca <30645648+jemtca@users.noreply.github.com>
2024-07-24 19:47:37 -0700

    CodingBat

--- JavaScript/Functional-2/noZ.js
+++ JavaScript/Functional-2/noZ.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of strings, return a list of the strings, omitting any string that contains a 'z'
+const noZ = (strings) => {
+    const newList = strings.filter(n => !n.includes('z'));
+
+    return newList;
+};
+
+console.log(noZ(['aaa', 'bbb', 'aza']));
+console.log(noZ(['hziz', 'hzello', 'hi']));
+console.log(noZ(['hello', 'howz', 'are', 'youz']));
