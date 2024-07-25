412154140f13388ab761a586344cebd81dac34ef
jemtca <30645648+jemtca@users.noreply.github.com>
2024-07-25 16:54:14 -0700

    CodingBat

--- JavaScript/Functional-2/noLong.js
+++ JavaScript/Functional-2/noLong.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of strings, return a list of the strings, omitting any string length 4 or more
+const noLong = (strings) => {
+    const newList = strings.filter(n => n.length < 4);
+
+    return newList;
+};
+
+console.log(noLong(['this', 'not', 'too', 'long']));
+console.log(noLong(['a', 'bbb', 'cccc']));
+console.log(noLong(['cccc', 'cccc', 'cccc']));
