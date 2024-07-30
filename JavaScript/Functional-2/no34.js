51bca9514c424da84450758a196fd455ef508565
jemtca <30645648+jemtca@users.noreply.github.com>
2024-07-29 22:21:55 -0700

    CodingBat

--- JavaScript/Functional-2/no34.js
+++ JavaScript/Functional-2/no34.js
@@ -1,2 +1,11 @@
 
-// 
+// given a list of strings, return a list of the strings, omitting any string length 3 or 4
+const no34 = (strings) => {
+    const newList = strings.filter(n => n.length < 3 || n.length > 4);
+
+    return newList;
+};
+
+console.log(no34(['a', 'bb', 'ccc']));
+console.log(no34(['a', 'bb', 'ccc', 'dddd']));
+console.log(no34(['ccc', 'dddd', 'apple']));
