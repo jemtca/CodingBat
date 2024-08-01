709b4d9ffa5da4890b37548ba49cd028d5edee27
jemtca <30645648+jemtca@users.noreply.github.com>
2024-07-31 20:45:02 -0700

    CodingBat

--- JavaScript/Functional-2/noYY.js
+++ JavaScript/Functional-2/noYY.js
@@ -1,2 +1,13 @@
 
-// 
+// given a list of strings
+// return a list where each string has 'y' added at its end, omitting any resulting strings that contain 'yy' as a substring anywhere
+const noYY = (strings) => {
+    const newList1 = strings.map(n => n + 'y');
+    const newList2 = newList1.filter(n => !n.includes('yy'));
+    
+    return newList2;
+};
+
+console.log(noYY(['a', 'b', 'c']));
+console.log(noYY(['a', 'b', 'cy']));
+console.log(noYY(['xx', 'ya', 'zz']));
