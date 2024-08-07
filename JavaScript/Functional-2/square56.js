ed736c64ad6e7ac898e1608a7d6e9674fa3c14d2
jemtca <30645648+jemtca@users.noreply.github.com>
2024-08-06 21:53:01 -0700

    CodingBat

--- JavaScript/Functional-2/square56.js
+++ JavaScript/Functional-2/square56.js
@@ -1,2 +1,12 @@
 
-// 
+// given a list of integers,
+// return a list of those numbers squared and the product added to 10, omitting any of the resulting numbers that end in 5 or 6
+const square56 = (nums) => {
+    const newList = nums.map(n => (n * n) + 10).filter(n => n % 10 !== 5 && n % 10 !== 6);
+
+    return newList;
+};
+
+console.log(square56([3, 1, 4]));
+console.log(square56([1]));
+console.log(square56([2]));
